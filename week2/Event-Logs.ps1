762726a69dc0c0936991ca8609e2858bbe82c8df
JosephMayfield <113706426+JosephMayfield@users.noreply.github.com>
2024-10-23 14:22:36 -0400

    Add files via upload

---
+++ week2/Event-Logs.ps1
@@ -0,0 +1,5 @@
+﻿
+#Get login and logoff records from windows Events
+Get-EventLog System -source Microsoft-Windows-Winlogon
+
+
---
+++ week2/Main.ps1
@@ -0,0 +1,15 @@
+﻿. (Join-Path $PSScriptRoot String-Helper.ps1)
+
+clear
+
+#Get Login and Logoffs from the last 15 days
+$loginoutTable = getLogInAndOffs 40;
+$loginoutTable
+
+# Get Shut Downs from the last 25 days
+$shutdownsTable = getShutDowns 40;
+$shutdownsTable
+
+# Get Start Ups from the last 25 days
+$startupsTable = getStartUps 40;
+$startupsTable 
