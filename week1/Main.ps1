7871c972220377660690d888a904eb9cea300821
JosephMayfield <113706426+JosephMayfield@users.noreply.github.com>
2024-10-25 13:26:42 -0400

    Add files via upload

---
+++ week1/Main.ps1
@@ -0,0 +1,25 @@
+﻿. (Join-Path $PSScriptRoot String-Helper.ps1)
+. (Join-Path $PSScriptRoot AssignmentFunctions.ps1)
+
+ApacheLogs2("index.html","200","Chrome")
+
+clear
+
+#Get Login and Logoffs from the last 15 days
+$loginoutTable = getLogInAndOffs 40;
+#$loginoutTable
+
+# Get Shut Downs from the last 25 days
+$shutdownsTable = getShutDowns 40;
+#$shutdownsTable
+
+# Get Start Ups from the last 25 days
+$startupsTable = getStartUps 40;
+#$startupsTable 
+
+
+<#
+$tableLogs = ApacheLogs1
+$tableLogs | Format-Table -AutoSize -Wrap
+#>
+
