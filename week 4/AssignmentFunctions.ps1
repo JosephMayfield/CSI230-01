ca48294697a06d00837aa523777cad1367a7afff
JosephMayfield <113706426+JosephMayfield@users.noreply.github.com>
2024-10-25 13:40:22 -0400

    Add files via upload

---
+++ week 4/AssignmentFunctions.ps1
@@ -0,0 +1,17 @@
+﻿function ApacheLogs2($page, $httpc, $browser){
+
+$logsNotformatted = Get-Content C:\xampp\apache\logs\access.log | `
+Select-String "/$page " | Select-String " $httpc " | Select-String " $browser/"
+
+return $logsNotformatted
+}
+
+# $logsNotformatted = ApacheLogs1 "*" "404" "Chrome"
+# $logsNotformatted
+
+
+# --------------------------------------------------------------------
+# Parsing Apache Logs Assignment 
+# (this is a more structured, better approach to analyzing apache logs.
+# They do the earlier assignments to learn Get-Content, Select-String)
+# --------------------------------------------------------------------
